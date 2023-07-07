804f014484982a30ab11b3796e5bc301dab8ed45
garo (they/them) <3411715+shrouxm@users.noreply.github.com>
2023-07-06 17:03:47 -0700

    feat: add project/site service/slice (#19)

--- src/config.ts
+++ src/config.ts
@@ -4,19 +4,11 @@ export type TerrasoAPIConfig = {
   terrasoAPIURL: string;
   graphQLEndpoint: string;
   tokenStorage: {
-    getToken: (name: string) => Promise<string | undefined>;
-    setToken: (name: string, token: string) => Promise<void>;
-    removeToken: (name: string) => Promise<void>;
-    /**
-     * Value of initial token used to init redux store.
-     * @remarks
-     * Redux requires a synchronous value to be passed to the store initial state.
-     * For environments where the token can be fetched synchronously, the token value
-     * can be passed. For environments where getToken is by default async, a null
-     * value should be provided. Application code is then responsible for initializing
-     * the token value on app startup.
-     */
-    initialToken: string | null;
+    getToken: (
+      name: string
+    ) => Promise<string | undefined> | string | undefined;
+    setToken: (name: string, token: string) => Promise<void> | void;
+    removeToken: (name: string) => Promise<void> | void;
   };
   logger: (severity: Severity, ...args: any[]) => void;
 };
---
+++ src/site/siteService.ts
@@ -0,0 +1,149 @@
+/*
+ * Copyright © 2023 Technology Matters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+import type { User } from 'terraso-client-shared/account/accountSlice';
+import { graphql } from 'terraso-client-shared/graphqlSchema';
+import type {
+  SiteAddMutationInput,
+  SiteDataFragment,
+  SiteUpdateMutationInput,
+} from 'terraso-client-shared/graphqlSchema/graphql';
+import type { Site } from 'terraso-client-shared/site/siteSlice';
+import * as terrasoApi from 'terraso-client-shared/terrasoApi/api';
+import { collapseConnectionEdges } from 'terraso-client-shared/terrasoApi/utils';
+
+const collapseSiteFields = (site: SiteDataFragment): Site => {
+  const { project, owner, ...rest } = site;
+  return {
+    ...rest,
+    projectId: project?.id,
+    ownerId: owner?.id,
+  };
+};
+
+export const fetchSite = (id: string) => {
+  const query = graphql(`
+    query site($id: ID!) {
+      site(id: $id) {
+        ...siteData
+      }
+    }
+  `);
+
+  return terrasoApi
+    .requestGraphQL(query, { id })
+    .then(resp => collapseSiteFields(resp.site));
+};
+
+export const fetchSitesForProject = (id: string) => {
+  const query = graphql(`
+    query sitesForProject($id: ID!) {
+      sites(project: $id) {
+        edges {
+          node {
+            ...siteData
+          }
+        }
+      }
+    }
+  `);
+
+  return terrasoApi
+    .requestGraphQL(query, { id })
+    .then(resp => collapseConnectionEdges(resp.sites).map(collapseSiteFields));
+};
+
+export const fetchSitesForUser = async (_: undefined, user: User | null) => {
+  if (user === null) {
+    return [];
+  }
+
+  const query = graphql(`
+    query userSites($id: ID!) {
+      userSites: sites(owner: $id) {
+        edges {
+          node {
+            ...siteData
+          }
+        }
+      }
+      projectSites: sites(project_Member: $id) {
+        edges {
+          node {
+            ...siteData
+          }
+        }
+      }
+    }
+  `);
+
+  return terrasoApi
+    .requestGraphQL(query, { id: user.id })
+    .then(resp =>
+      collapseConnectionEdges(resp.userSites)
+        .concat(collapseConnectionEdges(resp.projectSites))
+        .map(collapseSiteFields)
+    );
+};
+
+export const addSite = (site: SiteAddMutationInput) => {
+  const query = graphql(`
+    mutation addSite($input: SiteAddMutationInput!) {
+      addSite(input: $input) {
+        site {
+          ...siteData
+        }
+        errors
+      }
+    }
+  `);
+
+  return terrasoApi
+    .requestGraphQL(query, { input: site })
+    .then(resp => collapseSiteFields(resp.addSite.site));
+};
+
+export const updateSite = (site: SiteUpdateMutationInput) => {
+  const query = graphql(`
+    mutation updateSite($input: SiteUpdateMutationInput!) {
+      updateSite(input: $input) {
+        site {
+          ...siteData
+        }
+        errors
+      }
+    }
+  `);
+
+  return terrasoApi
+    .requestGraphQL(query, { input: site })
+    .then(resp => collapseSiteFields(resp.updateSite.site!));
+};
+
+export const deleteSite = (site: Site) => {
+  const query = graphql(`
+    mutation deleteSite($input: SiteDeleteMutationInput!) {
+      deleteSite(input: $input) {
+        errors
+      }
+    }
+  `);
+
+  return terrasoApi
+    .requestGraphQL(query, { input: { id: site.id } })
+    .then(_ => site.id);
+};
