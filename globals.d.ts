8cb596da63b159be96027823fc23e9657351d1fb
Paul Schreiber <paul@techmatters.org>
2023-10-19 14:13:59 -0400

    fix: specify path to SVGs

--- globals.d.ts
+++ globals.d.ts
@@ -1,4 +1,4 @@
-declare module '*.svg' {
+declare module 'src/assets/*.svg' {
   import * as React from 'react';
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
