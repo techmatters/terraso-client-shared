8d33df0f87b386927ef6b43996b25e3fda4e548f
Paul Schreiber <paul@techmatters.org>
2023-10-19 14:20:26 -0400

    fix: remove logos.ts, globals.d.ts; remove reference from tsconfig

--- globals.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'assets/*.svg' {
-  import * as React from 'react';
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-  export default content;
-}
--- src/logos.ts
+++
@@ -1,5 +0,0 @@
-import GoogleLogo from 'terraso-client-shared/assets/google.svg';
-import MicrosoftLogo from 'terraso-client-shared/assets/microsoft.svg';
-
-export const GoogleSvg = GoogleLogo;
-export const MicrosoftSvg = MicrosoftLogo;
