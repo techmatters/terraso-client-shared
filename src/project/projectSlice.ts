e73aa26cd62226b895769956fe22ded073a87c3d
Aja Beckett <ajabeckett@gmail.com>
2023-11-02 14:39:28 -0700

    fix: siteInstructions type

--- src/project/projectSlice.ts
+++ src/project/projectSlice.ts
@@ -45,7 +45,7 @@ export type Project = {
   name: string;
   privacy: 'PRIVATE' | 'PUBLIC';
   description: string;
-  siteInstructions: string;
+  siteInstructions?: string;
   updatedAt: string; // this should be Date.toLocaleDateString; redux can't serialize Dates
   memberships: Record<string, ProjectMembership>;
   sites: SerializableSet;
