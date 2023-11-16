6cb1deb36704c9f3a4cc2e5e5e0d431c1e2ad80c
Jose Buitron <jose.buitron@gmail.com>
2023-11-16 16:38:25 -0500

    fix: Added membership types

--- src/collaboration/membershipsUtils.ts
+++ src/collaboration/membershipsUtils.ts
@@ -29,6 +29,25 @@ type MembershipQuery = Partial<
     CollaborationMembershipsPendingFragment
 >;
 
+export type MembershipList = {
+  // TODO: massage membershipsUtils/Service so more of these can be required
+  membershipsInfo?: {
+    totalCount?: number;
+    pendingCount?: number;
+    accountMembership?: Membership;
+    membersSample?: Membership[];
+  };
+  id: string;
+  slug: string;
+  membershipType: 'CLOSED' | 'OPEN';
+};
+
+export type Membership = {
+  membershipId: string;
+  membershipStatus: 'APPROVED' | 'PENDING';
+};
+
+
 export const extractMembershipsInfo = (
   membershipList?: MembershipQuery | null,
 ) => ({
