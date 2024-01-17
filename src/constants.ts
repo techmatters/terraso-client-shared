be38d75cb850e4ff95e243e23fffa6f60c298b07
David Code Howard <me@davidcode.ca>
2024-01-16 20:30:26 -0500

    feat: Generate project intervals on frontend (#248)

---
+++ src/constants.ts
@@ -0,0 +1,23 @@
+type DepthIntervalBounds = {
+  start: number;
+  end: number;
+};
+
+export const PRESETS: Record<'LANDPKS' | 'NRCS', DepthIntervalBounds[]> = {
+  LANDPKS: [
+    { start: 0, end: 10 },
+    { start: 10, end: 20 },
+    { start: 20, end: 50 },
+    { start: 50, end: 70 },
+    { start: 70, end: 100 },
+    { start: 100, end: 200 },
+  ],
+  NRCS: [
+    { start: 0, end: 5 },
+    { start: 5, end: 15 },
+    { start: 15, end: 30 },
+    { start: 30, end: 60 },
+    { start: 60, end: 100 },
+    { start: 100, end: 200 },
+  ],
+};
