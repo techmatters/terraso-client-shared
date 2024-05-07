0d84e1cbfd06c84b21d703c448bd251ade0382e2
Paul Schreiber <paul@techmatters.org>
2024-05-07 18:26:31 -0400

    fix: make elevation optional

--- src/site/siteSlice.ts
+++ src/site/siteSlice.ts
@@ -40,7 +40,7 @@ export type Site = {
   name: string;
   latitude: number;
   longitude: number;
-  elevation: number;
+  elevation?: number;
   privacy: SitePrivacy;
   archived: boolean;
   updatedAt: string;
