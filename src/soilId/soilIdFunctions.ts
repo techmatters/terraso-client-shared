c7fe1257b9c2b591bbf20b4e9ad0884fd7e78c82
Ruxandra Machedon <ruxandra@techmatters.org>
2024-05-24 12:22:38 -0400

    feat: add data conversions needed for soil ID input

--- src/soilId/soilIdFunctions.ts
+++ src/soilId/soilIdFunctions.ts
@@ -15,7 +15,22 @@
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
-import { CollectionMethod, DepthInterval, SoilPitMethod } from './soilIdTypes';
+import { mhvcToLab } from 'munsell';
+import {
+  LabColorInput,
+  Maybe,
+  SoilIdInputData,
+  SoilIdInputDepthDependentData,
+  SoilIdSoilDataSlopeSteepnessSelectChoices,
+} from 'terraso-client-shared/graphqlSchema/graphql';
+
+import {
+  CollectionMethod,
+  DepthDependentSoilData,
+  DepthInterval,
+  SoilData,
+  SoilPitMethod,
+} from './soilIdTypes';
 
 export const methodEnabled = <T extends SoilPitMethod>(
   method: T,
@@ -39,3 +54,86 @@ export const compareInterval = (
   { depthInterval: a }: { depthInterval: DepthInterval },
   { depthInterval: b }: { depthInterval: DepthInterval },
 ) => a.start - b.start;
+
+export const degreeToPercent = (degrees: number) =>
+  Math.round(Math.tan((degrees * Math.PI) / 180) * 100);
+
+export const selectToPercent = (
+  select: SoilIdSoilDataSlopeSteepnessSelectChoices,
+) => {
+  switch (select) {
+    /** 0 - 2% (flat) */
+    case 'FLAT':
+      return 0.0;
+    /** 2 - 5% (gentle) */
+    case 'GENTLE':
+      return 0.02;
+    /** 15 - 30% (hilly) */
+    case 'HILLY':
+      return 0.15;
+    /** 5 - 10% (moderate) */
+    case 'MODERATE':
+      return 0.05;
+    /** 50 - 60% (moderately steep) */
+    case 'MODERATELY_STEEP':
+      return 0.5;
+    /** 10 - 15% (rolling) */
+    case 'ROLLING':
+      return 0.1;
+    /** 30 - 50% (steep) */
+    case 'STEEP':
+      return 0.3;
+    /** 100%+ (steepest) */
+    case 'STEEPEST':
+      return 1.0;
+    /** 60 - 100% (very steep) */
+    case 'VERY_STEEP':
+      return 0.6;
+  }
+};
+
+export const soilDataSlopePercent = (
+  data: SoilData,
+): Maybe<number> | undefined => {
+  if (data.slopeSteepnessSelect) {
+    return selectToPercent(data.slopeSteepnessSelect!);
+  } else if (typeof data.slopeSteepnessDegree === 'number') {
+    return degreeToPercent(data.slopeSteepnessDegree);
+  } else {
+    return data.slopeSteepnessPercent;
+  }
+};
+
+export const soilDataLabColorInput = (
+  data: DepthDependentSoilData,
+): LabColorInput | undefined => {
+  if (
+    typeof data.colorHue !== 'number' ||
+    typeof data.colorValue !== 'number' ||
+    typeof data.colorChroma !== 'number'
+  ) {
+    return undefined;
+  }
+  const [L, A, B] = mhvcToLab(data.colorHue, data.colorValue, data.colorChroma);
+  return { L, A, B };
+};
+
+export const soilDataToIdInput = (data: SoilData): SoilIdInputData => {
+  return {
+    depthDependentData: data.depthDependentData.map(
+      soilDepthDependentDataToIdInput,
+    ),
+    slope: soilDataSlopePercent(data),
+  };
+};
+
+export const soilDepthDependentDataToIdInput = (
+  data: DepthDependentSoilData,
+): SoilIdInputDepthDependentData => {
+  return {
+    depthInterval: data.depthInterval,
+    colorLAB: soilDataLabColorInput(data),
+    rockFragmentVolume: data.rockFragmentVolume,
+    texture: data.texture,
+  };
+};
