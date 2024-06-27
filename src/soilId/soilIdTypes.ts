24a30a517ccc44b32c2c24ec993c95fac68e33a2
Paul Schreiber <paul@techmatters.org>
2024-06-27 17:05:30 -0400

    fix: disable unused collection methods (#570)
    * fix: disable unused collection methods
    * fix: remove soilStructure from DEFAULT_ENABLED_SOIL_PIT_METHODS
    * fix: add allSoilPitMethods and allCollectionMethods exports
    * fix: ProjectSoilSettings type now does not expect disabled methods (#688)
    * fix: comment out unused methods in tests
    ---------
    Co-authored-by: Carissa Knipe <carissa@techmatters.org>

--- src/soilId/soilIdTypes.ts
+++ src/soilId/soilIdTypes.ts
@@ -29,13 +29,13 @@ import type {
   SoilIdProjectSoilSettingsDepthIntervalPresetChoices,
   SoilIdSoilDataSurfaceCracksSelectChoices,
 } from 'terraso-client-shared/graphqlSchema/graphql';
+import { MethodRequired } from 'terraso-client-shared/soilId/soilIdSlice';
 import { Coords } from 'terraso-client-shared/types';
 
 export type LoadingState = 'loading' | 'error' | 'ready';
 
-export const soilPitMethods = [
-  'soilTexture',
-  'soilColor',
+export const soilPitMethods = ['soilTexture', 'soilColor'] as const;
+export const disabledSoilPitMethods = [
   'soilStructure',
   'carbonates',
   'ph',
@@ -43,18 +43,36 @@ export const soilPitMethods = [
   'soilOrganicCarbonMatter',
   'sodiumAdsorptionRatio',
 ] as const;
+
+export const allSoilPitMethods = [
+  ...soilPitMethods,
+  ...disabledSoilPitMethods,
+] as const;
+
 export const collectionMethods = [
   'slope',
   'verticalCracking',
   ...soilPitMethods,
+  'notes',
+] as const;
+
+export const disabledCollectionMethods = [
   'soilLimitations',
   'landUseLandCover',
   'photos',
-  'notes',
+  ...disabledSoilPitMethods,
+] as const;
+
+export const allCollectionMethods = [
+  ...collectionMethods,
+  ...disabledCollectionMethods,
 ] as const;
 
 export type SoilPitMethod = (typeof soilPitMethods)[number];
 export type CollectionMethod = (typeof collectionMethods)[number];
+
+export type DisabledCollectionMethod =
+  (typeof disabledCollectionMethods)[number];
 
 export { DepthInterval };
 export type LabelledDepthInterval = {
@@ -71,12 +89,16 @@ export type SoilData = Omit<
   depthDependentData: DepthDependentSoilData[];
 };
 export type ProjectDepthInterval = Omit<ProjectDepthIntervalNode, 'project'>;
-export type ProjectSoilSettings = Omit<
+export type AllProjectSoilSettings = Omit<
   ProjectSoilSettingsNode,
   'project' | 'depthIntervals'
 > & {
   depthIntervals: ProjectDepthInterval[];
 };
+export type ProjectSoilSettings = Omit<
+  AllProjectSoilSettings,
+  MethodRequired<DisabledCollectionMethod>
+>;
 
 export type SoilTexture = SoilIdDepthDependentSoilDataTextureChoices;
 export const textures = [
